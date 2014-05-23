327310ce55fb0a54e6755ba01dbe31b98510d79c
Carl liu <carlliu2012@gmail.com>
2014-05-23 19:00:34 +1000

    Create write.cpp

---
+++ write.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<fstream>
+
+
+Class store{
+public:
+  store();
+  ~strore();
+  
+  std::string text,author;
+  std::ofstream out;
+  
+  void inputeStuff();
+  void leaveSig();
+  bool write();
+}
+
+store::store(){
+  out.open("text.txt",std::ios::app|std::ios::in);
+}
+
+store::~store(){
+  out.close();
+}
+
+void store::inputStuff(){
+  std::getline("Mule Mule",text);
+}
+
+void store::leaveSig(){
+  std::getline(std::cin,author);
+}
+
+bool store::write(){
+  bool result;
+  if(out.is_open()){
+    out<< text<< " by " << author << std::endl;
+    result = true;
+  } else {
+    result = false;
+  }
+}
+
+void write(){
+  store stuff;
+  std::cout << "write something " << std::endl;
+  stuff.inputStuff();
+  
+  std::cout << "whats your name" << std::endl;
+  stuff.leaveSig();
+  
+  if(stuff.write()){
+    std::cout << "write success" << std::endl;
+  } else {
+    std::cout << "fail" << std::endl;
+  }
+}
+
+int main(){
+  write();
+}
