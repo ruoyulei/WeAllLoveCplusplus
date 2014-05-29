8b03b1f3b9808013504889b6ca2c2fa4c9df1288
defuyun <carlliu2012@gmail.com>
2014-05-29 23:52:19 +1000

    added questions

--- project_euler.c
+++ project_euler.c
@@ -13,8 +13,34 @@ void multiplesof3and5(){
     }
     printf("%d\n",sum);
 }
+
+/*Fibonacci*/
+void Fibonacci(){
+    int termOne = 1,termTwo = 1;
+    int sum = 0;
+    int count = 0;
+    while(termOne < 4000000){
+        printf("%d %d ",termOne,termTwo);
+        termOne = termOne+termTwo;
+        termTwo = termOne+termTwo;
+        if((termOne & 1) && termOne < 4000000){
+            sum += termOne;
+            count++;
+        }
+        if((termTwo & 1) && termTwo < 4000000){
+            sum += termTwo;
+            count++;
+        }
+    }
+    putchar('\n');
+    printf("number of even terms in the Fibonacci sequence = %d\n",count);
+    printf("the sum of the numbers is %d\n",sum);
+}
+
+/*Fibonacci 2*/
 int main(int argc,char *argv[]){
     multiplesof3and5();
+    Fibonacci();
     return EXIT_SUCCESS;
 }
 
